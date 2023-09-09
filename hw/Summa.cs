c0b38f2fba317a630281e89709a8f56baf704718
sHiNka20 <123722238+sHiNka20@users.noreply.github.com>
2023-09-09 12:52:14 +0300

    Add files via upload

---
+++ hw/Summa.cs
@@ -0,0 +1,23 @@
+﻿
+int vvod(string mess)
+{
+    Console.Write(mess);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+    
+int summa(int number)
+{
+    int rav = 0;
+    
+    while(number > 0)
+    {
+        rav += number % 10;
+        number /= 10;
+    }
+    return rav;
+}
+
+int number = vvod("Введите число : ");
+
+Console.WriteLine($" Сумма всех цифр в числе {number} = {summa(number)}");
