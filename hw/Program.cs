f00b2c2d9d5abfa9733cc84a5c01008759bb4a06
Стас <@DESKTOP-0OF5UJB>
2023-09-09 11:57:58 +0300

    Добавьте файлы проекта.

---
+++ hw/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Homework
+{
+    public static int stepen(int num, int secnum)
+    {
+        int itog = 0;
+        for (int i = 1; i < secnum; i++)
+        {
+            if (itog == 0)
+            {
+                itog = num * num;
+            }
+            else
+            {
+                itog = itog * num;
+            }
+        }
+        return itog;
+    }
+
+    public static void Main(string[] args)
+    {
+        int num;
+        int secnum;
+
+        Console.Write("Введите число которое хотите возвести :");
+        num = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите степень в которую хотите возвести данное число :");
+        secnum = Convert.ToInt32(Console.ReadLine());
+
+        int result = stepen(num, secnum);
+        Console.WriteLine($"  {num} В степени {secnum} = {result}");
+    }
+}
